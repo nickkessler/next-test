f701c7020067ba9e44e4ae7a65ba343a8f42bd8e
Nick Kessler <nkessler@activeingredients.com>
2023-11-17 12:35:55 -0800

    create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </main>
+  );
+}
+
+export default About;
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,12 @@ function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
+        <header>
+          <nav>
+            <a href='/'>Home</a>
+            <a href='/about'>About</a>
+          </nav>
+        </header>
         {children}
         <footer>Dogs are better than cats</footer>
       </body>
